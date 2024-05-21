ada3c3c503353dee1e1e310e7d23a66649e7f013
vikramkg82 <vikramkg82@gmail.com>
2024-05-21 15:40:46 -0400

    Loop problem

--- src/loop/ProblemLoop.java
+++ src/loop/ProblemLoop.java
@@ -52,18 +52,201 @@ public class ProblemLoop {
         Scanner console = new Scanner(System.in);
         return console;
      }
+
+    static int readNumber(){
+         int number = ConsoleInput().nextInt();
+         return  number;
+     }
     public static void main(String[] args) {
 
-        // sum of the digit
+         //palindrome();
+         //sumOfDigit();
+         //reverseNumber();
+         //factorialOfNumber();
+         //armStrongNumber();
+         //perfectNumber();
+           //primeNumber();
+        fibonacci();
 
-      int number = ConsoleInput().nextInt();;
-        System.out.println(number);
 
-        System.out.println(number/10);
-        System.out.println(number%10 );
-        System.out.println((number>>2)>>2>>2>>2);
-        System.out.println(number<<2);
+
+
+
+
 
 
     }
+
+
+    //      Check if a given number is a palindrome
+    static void palindrome(){
+        int number = readNumber();
+        int orginalNumber=number;
+        int reverseNumber = 0;
+        while (number != 0) {
+
+            reverseNumber = ((reverseNumber * 10) + number % 10);
+            number = number / 10;
+
+        }
+
+        if(reverseNumber==orginalNumber){
+            System.out.println("number is palindrome");
+        }
+
+        else{
+            System.out.println("NUmber is not palindrome");
+        }
+
+    }
+
+
+
+
+    // sum of the digit
+
+    static void sumOfDigit() {
+
+      int number = ConsoleInput().nextInt();
+      int sum=0;
+      while((number!=0)){
+          sum = sum+(number%10);
+          number=number/10;
+
+
+      }
+        System.out.println(sum);
+    }
+
+// reverse a number
+
+    static void reverseNumber(){
+        int number = readNumber();
+       int reverseNumber = 0;
+        while (number != 0) {
+
+            reverseNumber = ((reverseNumber * 10) + number % 10);
+            number = number / 10;
+
+        }
+
+        System.out.println("Reverse of the number: "+ reverseNumber);
+    }
+
+
+    //factorial of a number
+
+    static void factorialOfNumber(){
+        int number = readNumber();
+        int factorial = 1;
+        if(number >0) {
+
+            for (int i = 1; i <= number; i++) {
+                factorial *= i;
+
+            }
+
+            System.out.println("Factorial of the number" + number + " is" + factorial);
+        }
+        else
+            {
+                System.out.println("NUmber should be >0");
+            }
+
+
+        }
+
+
+       // check a number is armstrong
+
+      static void armStrongNumber(){
+
+        int number =readNumber();
+        int sum=0;
+        int originalNUmber = number;
+        while(number!=0){
+
+             int reminder = number%10;
+             sum += (reminder*reminder*reminder);
+             number =number/10;
+
+        }
+        if(sum==originalNUmber){
+            System.out.println("Number is armstrong");
+        }
+        else
+        {
+            System.out.println("Number is not a armstrong number");
+        }
+
+
+    }
+
+
+    // perfect number
+
+    static void perfectNumber(){
+        int number= readNumber();
+        int sum=1;
+        for (int i = 2; i <= number/2 ; i++) {
+
+            if((number%i)==0){
+                sum+=i;
+            }
+
+        }
+        if(sum==number){
+            System.out.println("Number is perfectNumber");
+        }
+        else{
+            System.out.println("NUmber is not a perfect number");
+        }
+    }
+
+
+    //prime number
+
+    static void primeNumber(){
+        int number = readNumber();
+        boolean found =false;
+        for (int i = 2; i <= number/2 ; i++) {
+
+            if(number%i == 0){
+                found = true;
+            }
+
+        }
+
+        if(found==false){
+            System.out.println("Entered number is a prime number");
+        }
+
+        else{
+            System.out.println("Number isn ot a prime number");
+        }
+    }
+
+
+
+    static  void fibonacci(){
+        int number = readNumber();  //0,1,1,2,3,5,8,13
+         int firstNumber=0;
+         int secondNumber=1;
+        System.out.print(firstNumber + ",");
+        System.out.print(secondNumber);//0
+        for (int i = 3; i < number; i++) {
+            secondNumber = firstNumber+secondNumber;
+            if(secondNumber>number){
+
+                break;
+            }
+            firstNumber=secondNumber-firstNumber;
+            System.out.print("," + secondNumber );
+
+
+        }
+
+    }
+
+
 }
