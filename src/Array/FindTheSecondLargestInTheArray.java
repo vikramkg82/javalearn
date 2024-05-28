74d3e21d802d9740e13297931d93187a4e2725df
vikramkg82 <vikramkg82@gmail.com>
2024-05-28 17:07:44 -0400

    Common Element between the Array updated

--- src/Array/FindTheSecondLargestInTheArray.java
+++ src/Array/FindTheSecondLargestInTheArray.java
@@ -3,21 +3,25 @@ package Array;
 public class FindTheSecondLargestInTheArray {
 
     public static void main(String[] args) {
-        int[] array ={-2,-3,0,-5,8};
+        //int[] array ={-3,2,10,5};
+       // int [] array ={10,1,2,3};
+        int [] array={1,2,3,5};
+        //int [] array={-2,4,10,5};
         //int[] array ={3};
         if(array.length>1){
             int index=0;
             int max = array[0];
-            int secondlargest=array[0];
+            int secondlargest=Integer.MIN_VALUE;
             for (int i = 1; i < array.length; i++) {
-                if(max<array[i]);
-                 index=i;
+                if(max<array[i])
+                { index=i;
+                max=array[i];}
             }
 
-            for (int i = 1; i <array.length ; i++) {
+            for (int i = 0; i <array.length ; i++) {
                 if(index!=i ){
 
-                    if(secondlargest<array[i]){
+                    if(secondlargest<array[i] && secondlargest<max){
                         secondlargest=array[i];
                     }
 
