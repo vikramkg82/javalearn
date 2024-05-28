d0784da9c022ca420cf8d1915fbf1be2975cca73
vikramkg82 <vikramkg82@gmail.com>
2024-05-28 17:07:22 -0400

    Common Element between the Array updated

--- src/Array/CommonElementBetweenTwoIntArray.java
+++ src/Array/CommonElementBetweenTwoIntArray.java
@@ -1,99 +1,43 @@
 package Array;
 
 import java.sql.SQLOutput;
+import java.util.Arrays;
 
 public class CommonElementBetweenTwoIntArray {
     public static void main(String[] args) {
 
-        int[] array1 = {0,1,5,4};
-        int[] array2 = {0,1,5,4};
+        int[] array1 = {0,1,5,4,8};
+        int[] array2 = {0,1,5,0,4,1,8,9};
         int totalCommonEle = 0; // number of common element between the array
-
-        if (array1.length > array2.length) {     // depending on the number of element in the array choose larger array to compare with the smaller array
-            int[] commonArray = new int[array2.length];  // array to hold the common element
-            int zeroCount = 0;
-            int indexOfCA = 0;                              // index for common element
-            for (int i = 0; i < array1.length; i++) {           // lager array
-                for (int j = 0; j < array2.length; j++) {     // smaller array
-
-                    if (array1[i] == array2[j]) {                // elements are equal
-                                        if(array1[i]==0){  //to check if the 2 array has zero as common element so that we can add that in the end to common array
-                                            zeroCount++;
-                                        }
-                        for (int k = 0; k < commonArray.length; k++) {  // check if that element already in the common array.
-
-                            if (commonArray[k] != array1[i]) {        // if element not present in the common array add it
+        int [] commonElement= new int[(Math.min(array1.length, array2.length))];
 
 
-                                commonArray[indexOfCA] = array1[i];
-                                indexOfCA++;
-                                totalCommonEle++;
-                            }
+
+        for (int i = 0; i < array1.length; i++) {
+            for (int j = 0; j < array2.length; j++) {
+                if(array1[i]==array2[j]){
+
+                    boolean alreadyExists=false;
+                    for (int k = 0; k < totalCommonEle; k++) {
+                        if(array1[i] == commonElement[k]){
+                            alreadyExists=true;
                             break;
-
                         }
-
                     }
-
-
+                    if(!alreadyExists){
+                        commonElement[totalCommonEle++]=array1[i];
+                    }
+                    break;
+                    
                 }
 
             }
-
-
-        if(zeroCount>0){
-            commonArray[totalCommonEle]=0; //if bot hte array contains the zero add that zero here.
-            totalCommonEle++;
-        }
-        System.out.println("Common Element in the Array");
-        for (int i = 0; i < totalCommonEle; i++) {
-            System.out.print(commonArray[i] + " ");
-
-        }
-    }
-        else
-
-    {
-        int[] commonArray = new int[array1.length];
-        int indexOfCA = 0;
-        int zeroCount=0;
-        for (int i = 0; i < array2.length; i++) {
-            for (int j = 0; j < array1.length; j++) {
-
-                if (array2[i] == array1[j]) {
-                    if(array2[i]==0){
-                        zeroCount++;
-                    }
-
-                    for (int k = 0; k < commonArray.length; k++) {
-
-                        if (commonArray[k] != array2[i]) {
-                            commonArray[indexOfCA] = array2[i];
-                            indexOfCA++;
-                            totalCommonEle++;
-                        }
-                        break;
-
-                    }
-
-
-                }
-
-            }
-
-        }
-        if(zeroCount>0){
-            commonArray[totalCommonEle]=0;
-            totalCommonEle++;
-        }
-        System.out.println("Common Element in the Array");
-        for (int i = 0; i < totalCommonEle; i++) {
-            System.out.print(commonArray[i] + " ");
-
+            
         }
 
-    }
 
+        System.out.println(Arrays.toString(commonElement));
+     
 }
     }
 
