35318738f45af3d726852f83a84fc29307b0da8d
vikramkg82 <vikramkg82@gmail.com>
2024-06-04 12:15:32 -0400

    Sum below and above the 2d array Diagonal Added right diagonal too

---
+++ src/Array/DynamicArray.java
@@ -0,0 +1,24 @@
+package Array;
+
+public class DynamicArray {
+
+    // insert , delete , element at a index
+    public static int[] array = new int[5];
+    public static int total=0;
+    public static void insert(int ele){
+        int counter=0;
+        if(total==0){
+
+            array[0]=ele;
+            total++;
+        }
+
+
+
+
+
+
+    }
+
+
+}
---
+++ src/junit/test.java
@@ -0,0 +1,14 @@
+package junit;
+
+public class test {
+    public static void main(String[] args) {
+        String a ="hi";
+
+
+    }
+
+
+    public static void f1(){
+        return;
+    }
+}
