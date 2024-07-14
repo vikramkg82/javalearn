f17b058a23a86dbbb1f370dc9b546dc01c2b148f
vikramkg82 <vikramkg82@gmail.com>
2024-07-14 16:04:48 -0400

    String problems

--- src/Map/SortedHasMap.java
+++ src/Map/SortedHasMap.java
@@ -4,12 +4,13 @@ import java.util.*;
 public class SortedHasMap {
      HashMap<String,Integer> map = new HashMap<>();
     public static void main(String[] args) {
-        String string = "Apart from counting words and characters, our online editor can help you to improve word choice and writing style, and, optionally, help you to detect grammar mistakes and plagiarism. To check word count, simply place your cursor into the text box above and start typing. You'll see the number of characters and words increase or decrease as you type, delete, and edit them. You can also copy and paste text from another program over into the online editor above. The Auto-Save feature will make sure you won't lose any changes while editing, even if you leave the site and come back later. ";
+        //String string = "Apart from counting words and characters, our online editor can help you to improve word choice and writing style, and, optionally, help you to detect grammar mistakes and plagiarism. To check word count, simply place your cursor into the text box above and start typing. You'll see the number of characters and words increase or decrease as you type, delete, and edit them. You can also copy and paste text from another program over into the online editor above. The Auto-Save feature will make sure you won't lose any changes while editing, even if you leave the site and come back later. ";
+        String string ="Hi hello hello 2 times and hi 2 times total time is not sure";
         HashMap<String,Integer> map = new HashMap<>();
         String[] array = string.split(" ");
          for (int i = 0; i < array.length; i++)
          {
-              if(map.containsKey(array[i])){
+              if(map.containsKey((array[i]).toLowerCase())){
                   map.put(array[i],map.get(array[i])+1);
               }
               else
@@ -25,7 +26,7 @@ public class SortedHasMap {
     public void ConvertHashMaptoList(Map<String,Integer> unsortedmap)
     {
         List<Map.Entry<String,Integer>> list = new ArrayList<>(unsortedmap.entrySet()); //convert map to list of map
-        list.sort(Map.Entry.comparingByValue()); // sort the list of map ;tried reversed() got error --need  Millan  magic here
+        list.sort(Map.Entry.comparingByValue()); // sort the map of list ;tried reversed() got error --need  Millan  magic here
         for (Map.Entry<String,Integer> entry : list) {
 
             System.out.println(entry.getKey() + "---->" + entry.getValue());
