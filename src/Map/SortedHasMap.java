10c208879172da807558c354a2f6506e8c3fbc10
vikramkg82 <vikramkg82@gmail.com>
2024-07-14 16:17:03 -0400

    String problems

--- src/Map/SortedHasMap.java
+++ src/Map/SortedHasMap.java
@@ -5,17 +5,17 @@ public class SortedHasMap {
      HashMap<String,Integer> map = new HashMap<>();
     public static void main(String[] args) {
         //String string = "Apart from counting words and characters, our online editor can help you to improve word choice and writing style, and, optionally, help you to detect grammar mistakes and plagiarism. To check word count, simply place your cursor into the text box above and start typing. You'll see the number of characters and words increase or decrease as you type, delete, and edit them. You can also copy and paste text from another program over into the online editor above. The Auto-Save feature will make sure you won't lose any changes while editing, even if you leave the site and come back later. ";
-        String string ="Hi hello hello 2 times and hi 2 times total time is not sure";
+        String string ="Hi hello hello 2 times and hi 2 times Hello total time is not sure";
         HashMap<String,Integer> map = new HashMap<>();
         String[] array = string.split(" ");
          for (int i = 0; i < array.length; i++)
          {
               if(map.containsKey((array[i]).toLowerCase())){
-                  map.put(array[i],map.get(array[i])+1);
+                  map.put((array[i].toLowerCase()),map.get(array[i].toLowerCase())+1);
               }
               else
               {
-                  map.put(array[i],1);
+                  map.put(array[i].toLowerCase(),1);
               }
         }
         SortedHasMap instance = new SortedHasMap();
